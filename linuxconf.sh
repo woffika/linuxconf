346a420328a0ebc2ca23c16cce8a2a586f14150f
Woffi <levente.fazekas1221@gmail.com>
2025-02-07 16:40:46 +0100

    kis bibi

--- linuxconf.sh
+++ linuxconf.sh
@@ -80,7 +80,7 @@ EOF
     wget http://www.webmin.com/jcameron-key.asc 
     sudo apt-key add jcameron-key.asc
     sudo apt update  
-    sudo apt install webmin
+    sudo apt install webmin -y
 
 # Apply packages settings
   echo ${bold}${yellow}Apply packages settings...${normal}
